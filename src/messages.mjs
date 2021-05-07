d6868e520073e89c29c90fb57304fdc6667d68d2
Alexis Guzman <alexis12989x@gmail.com>
2021-05-07 17:20:09 -0500

    Edit messages contet

--- src/messages.mjs
+++ src/messages.mjs
@@ -4,21 +4,23 @@ const name = 'Reexjs CLI';
 const repo = 'https://github.com/a12989x/reexjs-cli/issues';
 
 const messages = {
-    welcome: `Welcome to ${chalk.bold(
+    welcome: `Welcome to ${chalk.bold.magentaBright(
         name
-    )}! my custom react cli boilerplate. An interactive CLI automation toll for create react projects most fast and efficiently. ⚛️`,
+    )}! my custom react cli boilerplate. An interactive CLI automation tool for create react.js and next.js projects most fast and efficiently. ⚛️`,
     walkThrough:
-        'This utility will walk you through steps to configure a React boilerplate as per your requirements.',
+        'This utility will walk you through steps to configure a React.js or Next.js boilerplate as per your requirements.',
     complete: 'Setup is complete! You can check app in',
-    thanks: `Thanks for using ${chalk.bold(name)}. Happy Coding! </>`,
-    raiseIssue: `Found an issue? Raise here: ${chalk.green(repo)}`,
+    thanks: `Thanks for using ${chalk.bold.magentaBright(
+        name
+    )}. Happy Coding! </>`,
+    raiseIssue: `Found an issue? Raise here: ${chalk.bold.green(repo)}`,
 };
 
 const features = [
-    'Installs Dependencies and Dev Dependencies',
+    'React.js/Next.js setup',
     'Folder structure setup',
-    'Routing Setup',
-    'Use Context Default Setup',
+    'Routing/Pages setup',
+    'Installs Dependencies and Dev Dependencies',
 ];
 
 export { name, messages, features };
