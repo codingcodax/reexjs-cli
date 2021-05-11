00166f1ecc89b876ac88992184fbd9819327c4e3
Alexis Guzman <alexis12989x@gmail.com>
2021-05-11 12:55:54 -0500

    Create file to minify code for build script

---
+++ src/scripts/minify.mjs
@@ -0,0 +1,26 @@
+import glob from 'glob';
+import { minify } from 'terser';
+import { mkdirSync, readFileSync, writeFileSync } from 'fs';
+
+const cwd = process.cwd();
+
+const globOptions = { ignore: ['**/node_modules/**'] };
+const minifyOptions = { compress: true, mangle: true };
+
+glob('**/*.mjs', globOptions, (er, files) => {
+    files.map(async (file) => {
+        const fileDirectory = `${cwd}/${file.replace('src', 'lib')}`;
+        const folderDirectory = fileDirectory.slice(
+            0,
+            fileDirectory.lastIndexOf('/')
+        );
+        const data = readFileSync(file, 'utf8');
+        const minifyCode = await minify(data, minifyOptions);
+
+        mkdirSync(folderDirectory, { recursive: true }, (err) => {
+            if (err) throw err;
+        });
+
+        writeFileSync(fileDirectory, minifyCode.code);
+    });
+});
