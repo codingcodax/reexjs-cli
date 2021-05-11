041acf5514f1438ffc48c8ed7dbed2478e37d594
Alexis Guzman <alexis12989x@gmail.com>
2021-05-11 12:55:26 -0500

    Create index file for script

---
+++ src/scripts/index.mjs
@@ -0,0 +1,15 @@
+import setup from './setup.mjs';
+import installFramework from './installFramework.mjs';
+import reactRouting from './reactRouting.mjs';
+import nextPages from './nextPages.mjs';
+import writeFolderStructure from './writeFolderStructure.mjs';
+import installAllDependencies from './installAllDependencies.mjs';
+
+export {
+    setup,
+    installFramework,
+    reactRouting,
+    nextPages,
+    writeFolderStructure,
+    installAllDependencies,
+};
