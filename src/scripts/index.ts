1b1dfbf17b17cab9b7a5247f4e8b65d1a22353b0
Alexis Guzman <alexis12989x@gmail.com>
2021-10-07 13:08:28 -0500

    feat(scripts): set up index file

---
+++ src/scripts/index.ts
@@ -0,0 +1,7 @@
+export { default as installFramework } from './installFramework';
+export { default as writeReactRouting } from './writeReactRouting';
+export { default as createNextPages } from './createNextPages';
+export { default as writeFolderStructure } from './writeFolderStructure';
+export { default as writeStyleScripting } from './writeStyleScripting';
+export { default as installAllDependencies } from './installAllDependencies';
+export { default as handleInquirerSetup } from './handleInquirerSetup';
