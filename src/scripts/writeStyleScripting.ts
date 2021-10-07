df787d4ef2d2f20cf4b9b744297664e0adf9bde8
Alexis Guzman <alexis12989x@gmail.com>
2021-10-07 13:06:17 -0500

    feat(scripts): set up write style scripting file

---
+++ src/scripts/writeStyleScripting.ts
@@ -0,0 +1,11 @@
+import { WriteStyleScripting } from '../../types';
+
+const writeStyleScripting: WriteStyleScripting = (
+  styleScripting,
+  framework,
+  appDirectory
+) => {
+  console.log(styleScripting, framework, appDirectory);
+};
+
+export default writeStyleScripting;
