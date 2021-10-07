243a0044b72c487531680aada513bb4ce5575ef1
Alexis Guzman <alexis12989x@gmail.com>
2021-10-07 12:55:19 -0500

    feat(types): create write style scripting type

---
+++ types/scripts/writeStyleScripting.ts
@@ -0,0 +1,9 @@
+import { StyleScripting, Framework, AppDirectory } from '../';
+
+type WriteStyleScripting = (
+  styleScripting: StyleScripting,
+  framework: Framework,
+  appDirectory: AppDirectory
+) => void;
+
+export default WriteStyleScripting;
