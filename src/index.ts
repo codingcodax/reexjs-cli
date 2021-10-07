3e68b77811bc4e8bc51b4503263140dd54654103
Alexis Guzman <alexis12989x@gmail.com>
2021-10-07 13:09:29 -0500

    feat: set up index file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
-console.log('Hello World!');
+import { Command } from 'commander';
+
+import { version } from '../package.json';
+
+import { handleInquirerSetup } from './scripts';
+import setup from './setup';
+
+const program = new Command();
+
+const init = async () => {
+  program
+    .version(version)
+    .description(
+      'An interactive CLI automation tool 🛠️ for creating react.js and next.js projects most fast and efficiently. ⚛️'
+    );
+
+  program.parse();
+
+  try {
+    const {
+      appName,
+      framework,
+      isRoutingNeeded,
+      routes,
+      pages,
+      predefinedFolders,
+      additionalFolders,
+      styleScripting,
+      dependencies,
+      devDependencies,
+    } = await handleInquirerSetup();
+
+    setup(
+      appName,
+      framework,
+      isRoutingNeeded,
+      routes,
+      pages,
+      predefinedFolders,
+      additionalFolders,
+      styleScripting,
+      dependencies,
+      devDependencies
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+init();
