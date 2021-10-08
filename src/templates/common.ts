2168856d985dd6fb7c0f042974b0e74666208e65
Alexis Guzman <alexis12989x@gmail.com>
2021-10-08 09:49:03 -0500

    feat(templates): set up common file

---
+++ src/templates/common.ts
@@ -0,0 +1,6 @@
+const common = {
+  styles:
+    '* {\n    padding: 0;\n    margin: 0;\n    box-sizing: border-box;\n  }\n  \n  html,\n  body {\n    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,\n      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;\n  }\n  \n  a {\n    color: inherit;\n    text-decoration: none;\n  }\n',
+};
+
+export default common;
